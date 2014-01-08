b57d0587f531de95ca050a52bff5f2309d5f7dc8
Vicente Neto <vicente.neto@dce.ufpb.br>
2014-01-08 14:01:20 -0300

    My First App Completo

--- src/com/example/myfirstapp/MainActivity.java
+++ src/com/example/myfirstapp/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.myfirstapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,4 +23,13 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	/** Called when the user clicks the Send button */
+	public void sendMessage(View view) {
+		// Do something in response to button
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		EditText editText = (EditText) findViewById(R.id.edit_message);
+		String message = editText.getText().toString();
+		intent.putExtra(EXTRA_MESSAGE, message);
+		startActivity(intent);
+	}
 }
